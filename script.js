721b1024fc938270103f78529d296effa12f540b
faizansaiyed123 <fai1ggj@gmail.com>
2025-11-04 16:39:09 +0530

    changes in design and code also

---
+++ script.js
@@ -0,0 +1,7 @@
+const API_URL = "https://faizan-bot-worker.fai1ggj.workers.dev/"
+
+const chatBox = document.getElementById("chat-box");
+const input = document.getElementById("user-input");
+const sendBtn = document.getElementById("send-btn");
+
+function addMessage
